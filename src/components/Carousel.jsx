bd448e5e602c81a4dd95fc12b2d94cf58d954656
Abdugaffor-97 <abdurahimov.97@list.ru>
2020-11-27 10:31:45 +0500

    renoved spiner

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -69,20 +69,13 @@ class Sliders extends React.Component {
             <Carousel.Item key={index}>
               <div className='imagesContainer'>
                 {movies.map((movie) => (
-                  <>
-                    {movie.Poster ? (
-                      <Image
-                        key={movie.imdbID}
-                        className='image-item'
-                        src={movie.Poster}
-                        alt={movie.title}
-                        onClick={() => this.props.history.push('/details/' + movie.imdbID)}
-                      />
-                    ) : (
-                        <Spinner animation="grow" variant="light" />
-                      )}
-                  </>
-
+                  <Image
+                    key={movie.imdbID}
+                    className='image-item'
+                    src={movie.Poster}
+                    alt={movie.title}
+                    onClick={() => this.props.history.push('/details/' + movie.imdbID)}
+                  />
                 ))
                 }
               </div>
