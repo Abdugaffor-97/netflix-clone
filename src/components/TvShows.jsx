a40065988ab27aea6530d91cdd5688a4e90e6520
abdugaffor <abdurahimov.97@list.ru>
2020-12-13 16:54:17 +0500

    Little css bug fixed

--- src/components/TvShows.jsx
+++ src/components/TvShows.jsx
@@ -1,38 +1,39 @@
-import Sliders from './Carousel'
-const { Row, Col } = require("react-bootstrap")
-
-
+import Sliders from "./Carousel";
+const { Row, Col, Container } = require("react-bootstrap");
 
 const Shows = (props) => {
   return (
     <>
-      <Row className='text-white ml-4 mt-5'>
-        <Col md={6}>
-          <h1><b>TV Shows</b></h1>
-          <p>
-            These days, the small screen has some very big things to offer. From sitcoms to dramas to travel and talk shows, these are all the best programs on TV.
-            <a
-              className='text-white text-light border-bottom'
-              href="#aa"
-            > Watch now for free
-             </a>
-          </p>
-        </Col>
-      </Row>
+      <Container>
+        <Row className="text-white ml-4 mt-5">
+          <Col>
+            <h1>
+              <b>TV Shows</b>
+            </h1>
+            <p>
+              These days, the small screen has some very big things to offer.
+              From sitcoms to dramas to travel and talk shows, these are all the
+              best programs on TV.
+              <a className="text-white text-light border-bottom" href="#aa">
+                {" "}
+                Watch now for free
+              </a>
+            </p>
+          </Col>
+        </Row>
+      </Container>
       {props.searchQuery ? (
         <Sliders title={props.searchQuery} />
-      ) :
-        (<>
-          <Sliders title='The Crown' />
-          <Sliders title='hundred' />
-          <Sliders title='big sky' />
+      ) : (
+        <>
+          <Sliders title="The Crown" />
+          <Sliders title="hundred" />
+          <Sliders title="big sky" />
           <Sliders title="Grey's Anatomy" />
         </>
-        )
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
-
-export default Shows
+export default Shows;
